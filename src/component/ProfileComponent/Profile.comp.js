bd7fd7978b496d513561325f6dcfecf6d27de4e5
yogesh.bhattarai <yogesh.bhattarai@aitcentre.com>
2022-05-20 16:35:12 +0545

    Image Container For Profile

--- src/component/ProfileComponent/Profile.comp.js
+++ src/component/ProfileComponent/Profile.comp.js
@@ -2,45 +2,12 @@ import { Avatar } from "@mui/material";
 import React from "react";
 import classes from "./Profile.module.css";
 import { useSelector } from "react-redux";
+import ImageContainerComponent from "./ImageContainer/ImageContainer.comp";
 const ProfileComponent = () => {
   const user = useSelector((state) => state.user);
   return (
     <div className={classes.profile__component}>
-      <div className={classes.image__wrapper}>
-        <div className={classes.cover__image}>
-          <img
-            src="https://wallpaperaccess.com/full/2667331.jpg"
-            alt={"cover"}
-          />
-          <form>
-            <label for="cover">Upload</label>
-            <input id="cover" type="file" className="d-none" />
-          </form>
-        </div>
-        <div className={classes.profile__image}>
-          {user.image ? (
-            <Avatar
-              alt={user.username}
-              src={user.image}
-              variant="rounded"
-              sx={{ width: "100%", height: "100%" }}
-              className={classes.image__avatar}
-            />
-          ) : (
-            <Avatar
-              variant="rounded"
-              sx={{ width: "100%", height: "100%" }}
-              className={classes.image__avatar}
-            >
-              {user.username?.charAt(0)}
-            </Avatar>
-          )}
-          <form>
-            <label for="profile">Upload</label>
-            <input id="profile" type="file" className="d-none" />
-          </form>
-        </div>
-      </div>
+      <ImageContainerComponent user={user} />
     </div>
   );
 };
