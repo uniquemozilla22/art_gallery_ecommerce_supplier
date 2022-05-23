9e8ef08c0971177757d725efb709e43735edc736
yogesh.bhattarai <yogesh.bhattarai@aitcentre.com>
2022-05-23 16:20:05 +0545

    Profile Componenet Changes

--- src/component/ProfileComponent/Profile.comp.js
+++ src/component/ProfileComponent/Profile.comp.js
@@ -1,13 +1,57 @@
 import { Avatar } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./Profile.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import ImageContainerComponent from "./ImageContainer/ImageContainer.comp";
+import DetailsContainerComponent from "./DefailtContainer/DetailsContainer.comp";
+import ProfileDataFetch from "../../store/actions/Profile/ProfileData.fetch";
+import { Spinner } from "react-bootstrap";
 const ProfileComponent = () => {
   const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [datas, setDatas] = useState(null);
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = async () => {
+    const data = await dispatch(ProfileDataFetch());
+    if (!data) return;
+    setDatas({ ...data });
+  };
+
+  const handleUserDataUpdate = (type, data) => {
+    if (type === "Account") {
+      setDatas({ ...datas, account: { ...datas.account, ...data } });
+    }
+    if (type === "Contact") {
+      setDatas({ ...datas, contact: { ...datas.contact, ...data } });
+    }
+  };
   return (
     <div className={classes.profile__component}>
-      <ImageContainerComponent user={user} />
+      <div className={classes.profileImage}>
+        <ImageContainerComponent user={user} />
+      </div>
+      <div className={classes.profileDetails}>
+        {datas ? (
+          <>
+            <DetailsContainerComponent
+              title={"Account"}
+              data={datas.account}
+              update={handleUserDataUpdate}
+            />
+            <DetailsContainerComponent
+              title={"Contact"}
+              data={datas.contact}
+              update={handleUserDataUpdate}
+            />
+          </>
+        ) : (
+          <Spinner />
+        )}
+      </div>
     </div>
   );
 };
--- src/store/handleError/Error.js
+++ src/store/handleError/Error.js
@@ -34,3 +34,33 @@ export const postErrorHandle = (dispatch, title, error) => {
 
   return false;
 };
+
+export const getErrorHandle = (dispatch, title, error) => {
+  dispatch(hideLoading());
+  if (error.response === undefined) {
+    dispatch(
+      ErrorMessage({
+        title,
+        message: "Network Error ! Cannot find a connection",
+      })
+    );
+  } else {
+    if (error.response.statusCode === 500) {
+      dispatch(
+        WarningMessage({
+          title,
+          message: error.response.data.message,
+        })
+      );
+    } else {
+      dispatch(
+        ErrorMessage({
+          title,
+          message: error.response.data.message,
+        })
+      );
+    }
+  }
+
+  return false;
+};
