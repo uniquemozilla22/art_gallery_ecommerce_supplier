6a789547569a69e7bc5fcef5d4f1ba1ed9edef1c
yogesh.bhattarai <yogesh.bhattarai@aitcentre.com>
2022-06-15 14:52:55 +0545

    Profile Information Intiation

--- src/component/ProfileInformation/ProfileInformation.comp.js
+++ src/component/ProfileInformation/ProfileInformation.comp.js
@@ -2,7 +2,14 @@ import React from "react";
 import classes from "./ProfileInformation.module.css";
 
 const ProfileInformationComponent = () => {
-  return <div>ProfileInformationComponent</div>;
+  return (
+    <div>
+      <div className={classes.title__container}>
+        <h3>Documentation</h3>
+      </div>
+      <div className={classes.verified__documents}></div>
+    </div>
+  );
 };
 
 export default ProfileInformationComponent;
