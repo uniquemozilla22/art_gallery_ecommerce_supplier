36ed09babf0b06fe92f4c3258606fe4ee7d230ac
yogesh.bhattarai <yogesh.bhattarai@aitcentre.com>
2022-07-04 15:05:37 +0545

    Product Banner Initialization

--- src/component/ProductBanner/ProductBanner.comp.js
+++ src/component/ProductBanner/ProductBanner.comp.js
@@ -1,7 +1,62 @@
+import styled from "@emotion/styled";
+import { Tooltip } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router";
 
-const ProductBannerComponent = () => {
-  return <div>ProductBannerComponent</div>;
+const ProductBannerComponent = ({ data }) => {
+  const navigation = useNavigate();
+
+  const ProductNavigation = (id) => navigation("/products/" + id);
+  return (
+    <Wrapper>
+      <ConentContainer>
+        <Title>
+          <Span>WOW!</Span>
+          <br />
+          {data.title}
+        </Title>
+        <Paragraph>{data.content}</Paragraph>
+      </ConentContainer>
+      <ProductContainer>
+        <Tooltip title={data.product.name}>
+          <Image
+            src={data.product.image}
+            onClick={() => ProductNavigation(data.product.id)}
+          ></Image>
+        </Tooltip>
+      </ProductContainer>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div({
+  display: "flex",
+  alignItems: "center",
+  gap: "2rem",
+});
+const ConentContainer = styled.div({
+  flex: "3",
+});
+const ProductContainer = styled.div({
+  flex: "1",
+});
+
+const Title = styled.h2({
+  fontWeight: "700",
+  fontSize: "1.5rem",
+});
+
+const Span = styled.span({
+  color: "#FF5959",
+});
+
+const Paragraph = styled.p({
+  fontWeight: "400",
+  fontSize: "1.1rem",
+});
+
+const Image = styled.img({
+  borderRadius: "1rem",
+});
 
 export default ProductBannerComponent;
--- src/pages/Product/Home/ProductHome.page.js
+++ src/pages/Product/Home/ProductHome.page.js
@@ -1,7 +1,29 @@
-import React from "react";
+import styled from "@emotion/styled";
+import React, { useState } from "react";
+import ProductBannerComponent from "../../../component/ProductBanner/ProductBanner.comp";
 
 const ProductHomePage = () => {
-  return <div>ProductHomePage</div>;
+  const [data, setData] = useState({
+    banner: {
+      title: "This is your start Product",
+      content:
+        "This product has become one of the most intresting product looking at the engagements from the product. You are one of the best in terms of likes, we wish you would have more success on the product. ",
+
+      product: {
+        name: "Product 1",
+        id: 1,
+        image:
+          "https://media.istockphoto.com/photos/the-painter-hands-picture-id1190200652?k=20&m=1190200652&s=612x612&w=0&h=XeXAJt5Q-ieQSM1B8l4EB2qKFPrkgI-ceP9wfFspZUc=",
+      },
+    },
+  });
+  return (
+    <Wrapper>
+      <ProductBannerComponent data={data.banner} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div({});
 
 export default ProductHomePage;
