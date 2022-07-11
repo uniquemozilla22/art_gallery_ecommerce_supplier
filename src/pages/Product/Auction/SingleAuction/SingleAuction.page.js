0a156f7917182f1f3006fee136d671d85faf0238
yogesh.bhattarai <yogesh.bhattarai@aitcentre.com>
2022-07-11 16:40:57 +0545

    Appended the Tags and Categories

--- src/pages/Product/Auction/SingleAuction/SingleAuction.page.js
+++ src/pages/Product/Auction/SingleAuction/SingleAuction.page.js
@@ -1,5 +1,11 @@
 import styled from "@emotion/styled";
+import {
+  FavoriteBorderOutlined,
+  HeartBrokenOutlined,
+} from "@mui/icons-material";
+import { fontSize } from "@mui/system";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SingleAuctionPage = () => {
   return (
@@ -14,20 +20,32 @@ const SingleAuctionPage = () => {
         <DetailsWrapper>
           <Title>One</Title>
           <BiddingWrapper>
-            <Ending>
+            <BiddingDetail>
               Ending
               <Span> 4 Days 13 Hours 8 mins</Span>
-            </Ending>
-            <Estimate>
+            </BiddingDetail>
+            <BiddingDetail>
               Estimate <Span>12,000 - 20,000 NPR</Span>
-            </Estimate>
-            <Current>
+            </BiddingDetail>
+            <BiddingDetail>
               Current <Span>14,000 NPR</Span>
-            </Current>
+            </BiddingDetail>
           </BiddingWrapper>
+          <DetailDescription>THis is the description Part</DetailDescription>
+          <Like>
+            <FavoriteBorderOutlined /> 99
+          </Like>
+          <HyperLink>
+            <SubTitle>Tags</SubTitle>
+            <Links></Links>
+          </HyperLink>
+          <HyperLink>
+            <SubTitle>Category</SubTitle>
+            <Links></Links>
+          </HyperLink>
         </DetailsWrapper>
       </ContentWrapper>
-      <DescriptionWrapper></DescriptionWrapper>
+      <DescriptionWrapper>THis is the description Part</DescriptionWrapper>
     </Wrapper>
   );
 };
@@ -40,41 +58,61 @@ const ContentWrapper = styled.div({
   alignItems: "center",
   gap: "5rem",
   flexWrap: "wrap",
+  minHeight: "90vh",
 });
 const ImageWrapper = styled.div({
   flex: "1",
   display: "grid",
   placeItems: "center",
+  borderRight: "1px solid #CACACA",
+  height: "100%",
 });
 const DetailsWrapper = styled.div({
   flex: "1",
+  height: "100%",
+  display: "flex",
+  justifyContent: "space-evenely",
+  gap: "1rem",
+  flexDirection: "column",
 });
 
 const BiddingWrapper = styled.div({});
 
+const Like = styled.div({});
+
+const HyperLink = styled.div({});
+
 const Image = styled.img({});
+
+const DetailDescription = styled.p({
+  fontSize: "1rem",
+  fontWeight: "300",
+});
 
 const Title = styled.h2({
   fontSize: "2rem",
   fontWeight: "700",
 });
-const Ending = styled.p({
+
+const BiddingDetail = styled.p({
   fontSize: "1rem",
   fontWeight: "700",
   margin: "0",
-});
-const Estimate = styled.p({
-  fontSize: "1rem",
-  fontWeight: "700",
-  margin: "0",
+  display: "flex",
+  gap: "2rem",
 });
 
-const Current = styled.p({
-  fontSize: "1rem",
+const SubTitle = styled.h3({
   fontWeight: "700",
-  margin: "0",
+  fontSize: "1.5rem",
 });
+
+const Links = styled.div({});
+
+const Linked = styled(Link);
+
 const Span = styled.span({
   fontWeight: "400",
+  textAlign: "right",
 });
 export default SingleAuctionPage;
